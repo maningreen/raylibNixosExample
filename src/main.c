f65034db288f4dc63fc69f581abae18b1e573641
maningreen <flibbers@protonmail.com>
2025-02-28 13:44:07 -0700

    updated a few things in the source

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include "include.h"
 
 int main() {
-  InitWindow(600, 600, "TEST");
+  InitWindow(600, 600, "You should change this :thumbsup:");
   while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    EndDrawing();
   }
   CloseWindow();
   return 0;
